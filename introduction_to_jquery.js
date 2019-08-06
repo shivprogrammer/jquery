f1d2aedf181d98a7e6cd5843755ca4cd72e968d5
shivprogrammer <shiv.unadkat@gmail.com>
2019-08-05 21:56:52 -0700

    javascript version on how to add dynamic behavior to a web page

---
+++ introduction_to_jquery.js
@@ -0,0 +1,14 @@
+// javascript version of how to add dynamic behavior to a web page
+const login = document.getElementById('login');
+
+const loginMenu = document.getElementById('loginMenu');
+
+login.addEventListener('click', () => {
+    if (loginMenu.style.display === 'none') {
+        loginMenu.style.display = 'inline';
+    }
+    else {
+        loginMenu.style.display = 'none';
+    }
+})
+
